6deee1e06be62c91a458faf089ef6e9a5f3006ff
khaly <khalyruot12345@gmail.com>
2024-09-26 11:38:11 +0700

    Added my project

---
+++ src/about.js
@@ -0,0 +1,20 @@
+// about.js
+export function aboutPage(){
+
+    const content = document.createElement("p");
+    content.innerText = "Please search the contact below";
+    document.getElementById("content").appendChild(content);
+
+
+    let myArrayFooter = ["Khaly: 0967518934", "Tom: 097451321", "Dave: 0967321721"];
+    
+    //create textnode
+    for(let i = 0; i < myArrayFooter.length; i++){
+        var listArray = document.createElement("li");
+        const textToBeAdded = document.createTextNode(myArrayFooter[i]);
+        listArray.appendChild(textToBeAdded);
+        document.getElementById("content").appendChild(listArray);
+    }
+    
+
+}
