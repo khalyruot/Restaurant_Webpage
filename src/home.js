fe6c9f88c531221791de00d674f795fde46fad64
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-01 15:42:04 +0700

    Your commit message describing the changes

--- src/home.js
+++ src/home.js
@@ -2,32 +2,12 @@
 import restaurant from "./kong_kitchen_background.jpg"
 
 export function homePage(){
-
-    /*const image = document.createElement("img");
-    image.src = kong_kitchen_background.jpg;
-    document.getElementById("content").style.backgroundImage = ;
-
-    const contentElement = document.body;
-    contentElement.style.backgroundImage = `url(${restaurant})`;
-    contentElement.style.backgroundSize = "cover";  // This makes the image cover the entire element
-    contentElement.style.backgroundPosition = "center"; // Optional: centers the image in the container
-    contentElement.style.backgroundRepeat = "no-repeat";*/
     
 
     const content = document.createElement("p");
-    content.innerText = "Welcome to KongFu Kitchen";
+    content.innerText = "Enjoy Our Delicious Meals!!!";
+    content.setAttribute("id","content_homepage");
     document.getElementById("text_content").appendChild(content);
-
-
-    let myArrayFooter = ["Friendly Server", "Delicious Food", "Reasonable Price"];
-    
-    //create textnode
-    for(let i = 0; i < myArrayFooter.length; i++){
-        var listArray = document.createElement("li");
-        const textToBeAdded = document.createTextNode(myArrayFooter[i]);
-        listArray.appendChild(textToBeAdded);
-        document.getElementById("text_content").appendChild(listArray);
-    }
     
 
 }
