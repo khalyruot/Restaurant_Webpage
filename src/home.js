1607351ec2dca0d49547fc9dfaf3ee560eeb1e0d
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-02 13:12:18 +0700

    add 3 main point for list

--- src/home.js
+++ src/home.js
@@ -7,6 +7,7 @@ export function homePage(){
     const content = document.createElement("p");
     content.innerText = "Enjoy Our Delicious Meals!!!";
     content.setAttribute("id","content_homepage");
+    console.log(document.getElementById("text_content"));
     document.getElementById("text_content").appendChild(content);
     
 
