cc3074dd76e10ed52f030fb847a0f0a9a0e11402
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-03 15:47:22 +0700

    update top nav

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+        test: /\.html$/i,
+        loader: "html-loader", 
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
